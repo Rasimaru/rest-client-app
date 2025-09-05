d0896210d3b6a8b6c4b390fef245ddde471068f2
Dzmitry Drevich <jeidan2012@gmail.com>
2025-09-06 01:03:00 +0200

    chore(styles): make html, body full height, add temp wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex justify-center items-center h-full">{children}</div>
+      </body>
     </html>
   );
 }
