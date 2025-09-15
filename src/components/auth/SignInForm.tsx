0f7212a10417f19bdc94185ee054546e4ef23453
Dzmitry Drevich <jeidan2012@gmail.com>
2025-09-15 13:40:13 +0200

    feat(auth): add NextAuth support

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -8,14 +8,15 @@ import { ROUTES } from '@/lib/routes';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
-import { redirect } from 'next/navigation';
-import GoogleIcon from '../shared/icons/GoogleIcon';
+import { useRouter } from 'next/navigation';
+import GithubIcon from '../shared/icons/Github';
 import { toast } from 'sonner';
-import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { AuthSchema } from '@/lib/validationSchemas';
 import FieldWrapper from '../shared/FieldWrapper';
+import { signIn } from 'next-auth/react';
 
 export function SignInForm() {
+  const router = useRouter();
   const formSchema = AuthSchema;
 
   const form = useForm<Yup.InferType<typeof AuthSchema>>({
@@ -26,14 +27,17 @@ export function SignInForm() {
     }
   });
 
-  function onSubmit(values: Yup.InferType<typeof formSchema>) {
-    try {
-      console.log(values);
+  async function onSubmit(values: Yup.InferType<typeof formSchema>) {
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false
+    });
+
+    if (result?.error) {
+      toast.error(result.error === 'CredentialsSignin' ? 'Invalid credentials' : result.error);
+    } else {
       toast.success(`Signed in as ${values.email}`);
-      redirect(ROUTES.main);
-    } catch (error) {
-      if (isRedirectError(error)) return;
-      toast.error((error as Error).message || 'Sign In failed');
+      router.push(ROUTES.main);
     }
   }
 
@@ -46,7 +50,7 @@ export function SignInForm() {
             Let&apos;s get started. Sign In into your account with Email and Password.
           </CardDescription>
         </CardHeader>
-        <CardContent>
+        <CardContent className="flex flex-col gap-3">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <div className="grid gap-6">
@@ -70,7 +74,7 @@ export function SignInForm() {
                     <Link
                       href="#"
                       className="ml-auto inline-block text-sm underline"
-                      onClick={() => toast.info('Feature in development')}
+                      onClick={() => toast.info("Feature isn't ready yet")}
                     >
                       Forgot your password?
                     </Link>
@@ -79,26 +83,32 @@ export function SignInForm() {
                     Sign In
                   </Button>
                 </div>
-                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                  <span className="bg-card text-muted-foreground relative z-10 px-2">
-                    Or continue with
-                  </span>
-                </div>
-                <div className="flex flex-col gap-4">
-                  <Button variant="outline" className="w-full cursor-pointer">
-                    <GoogleIcon className="flex" />
-                    Sign In with Google
-                  </Button>
-                </div>
-                <div className="text-center text-sm">
-                  Don&apos;t have an account?{' '}
-                  <Link href={ROUTES.signup} className="underline underline-offset-4">
-                    Sign Up
-                  </Link>
-                </div>
               </div>
             </form>
           </Form>
+          <div className="flex flex-col gap-3">
+            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+              <span className="bg-card text-muted-foreground relative z-10 px-2">
+                Or continue with
+              </span>
+            </div>
+            <div className="flex flex-col gap-4">
+              <Button
+                variant="outline"
+                className="w-full cursor-pointer"
+                onClick={() => signIn('github', { callbackUrl: ROUTES.main })}
+              >
+                <GithubIcon className="flex" />
+                Sign In with Github
+              </Button>
+            </div>
+            <div className="text-center text-sm">
+              Don&apos;t have an account?{' '}
+              <Link href={ROUTES.signup} className="underline underline-offset-4">
+                Sign Up
+              </Link>
+            </div>
+          </div>
         </CardContent>
       </Card>
     </div>
