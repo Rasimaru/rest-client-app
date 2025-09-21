5bcec03897c3476d928811d02b4c6bb8b97faf13
Dzmitry Drevich <jeidan2012@gmail.com>
2025-09-21 21:33:20 +0200

    fix(routes): possibe fix for routes and redirects

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { handlers } from '@/auth';
 
-export const { GET, POST } = handlers;
+export async function OPTIONS() {
+  const res = NextResponse.json({});
+  res.headers.set('Access-Control-Allow-Origin', 'https://rest-client-app-rosy-six.vercel.app');
+  res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  return res;
+}
+
+export async function GET(req: NextRequest) {
+  return handlers.GET(req);
+}
+
+export async function POST(req: NextRequest) {
+  return handlers.POST(req);
+}
